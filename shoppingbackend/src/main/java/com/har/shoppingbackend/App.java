96ff353f8c196976bad3ac5de2c88725eeb23f62
Hardik1104 <art.hardikpatel@gmail.com>
2018-05-17 16:33:39 -0400

    Project Setup

---
+++ shoppingbackend/src/main/java/com/har/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package com.har.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
